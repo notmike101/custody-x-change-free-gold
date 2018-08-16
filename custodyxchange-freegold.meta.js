d359259952dc66c838d7d776665a0227f22f0256
Mike Orozco <mike.orozco94+gitlab@gmail.com>
2018-08-16 01:43:46 +0000

    Add new file

---
+++ custodyxchange-freegold.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Custody-X-Change Free Gold
+// @namespace    notmike101/cxc-freegold
+// @version      1
+// @description  Give gold for free
+// @author       Mike Orozco
+// @match        https://app.custodyxchange.com/*
+// @grant        GM.xmlhttpRequest
+// @run-at       document-start
+//
+// @downloadURL  https://gitlab.com/notmike101/custody-x-change-free-gold/raw/master/custodyxchange-freegold.user.js
+// @updateURL    https://gitlab.com/notmike101/custody-x-change-free-gold/raw/master/custodyxchange-freegold.meta.js
+
+// ==/UserScript==
