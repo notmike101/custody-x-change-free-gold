731a01617e8a1868cdcb1e0283fa5075ca2a4b7a
Mike Orozco <mike.orozco94+gitlab@gmail.com>
2018-08-16 01:44:21 +0000

    Add new file

---
+++ custodyxchange-freegold.raw.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         Custody-X-Change Free Gold
+// @namespace    notmike101/cxc-freegold
+// @version      0.1
+// @description  Give gold for free
+// @author       Mike Orozco
+// @match        https://app.custodyxchange.com/*
+// @grant        GM.xmlhttpRequest
+// @run-at       document-start
+// ==/UserScript==
+
+var open_prototype = XMLHttpRequest.prototype.open,
+intercept_response = function(urlpattern, callback) {
+   XMLHttpRequest.prototype.open = function() {
+      arguments['1'].match(urlpattern) && this.addEventListener('readystatechange', function(event) {
+         if ( this.readyState === 4 ) {
+            var response = callback(event.target.responseText);
+            Object.defineProperty(this, 'response', {writable: true});
+            Object.defineProperty(this, 'responseText', {writable: true});
+            this.response = this.responseText = response;
+
+             console.log(this);
+         }
+      });
+      return open_prototype.apply(this, arguments);
+   };
+};
+
+intercept_response(/\api\/account/gim,(response) => {
+    var newResponse = response.replace(/"planId"\:[0-9]{4},/gim, '"planId":3001,')
+        .replace(/"activePlan"\:"[a-zA-Z]+",/gim, '"activePlan":"Gold",')
+        .replace(/"lifetimeLicense"\:(true|false),/gim, '"lifetimeLicense":true,')
+        .replace(/"calculationsDisplay"\:[0-9],/gim, '"calculationsDisplay":0,');
+    return newResponse;
+});
+
+(function() {
+    'use strict';
+
+    function addScript(text) {
+        const replacer = new RegExp(`n.prototype.hasGoldPermissions=function(){return this.testPackage([tp.PACKAGE_GOLD,tp.PACKAGE_PROFESSIONAL_GOLD])};`,'gim');
+        text = text.replace(replacer, "n.prototype.hasGoldPermissions=true;");
+
+        var newScript = document.createElement('script');
+        newScript.type = "text/javascript";
+        newScript.textContent = text;
+        document.head.appendChild(newScript);
+        console.log('script added');
+    }
+
+    window.addEventListener('beforescriptexecute', function(e) {
+        src = e.target.src;
+        if (src.search(/main\.bundle\.js/) != -1) {
+            e.preventDefault();
+            e.stopPropagation();
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: e.target.src,
+                onload: function(response) {
+                    addScript(response.responseText);
+                }
+            });
+        }
+    });
+})();
